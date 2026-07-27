24ffa9d63e0db6adc29e20993b6aaf19d43852d8
jayden <jaydend082208@gmail.com>
2026-07-27 13:57:39 -0400

    add navbar and colors to

---
+++ gdytproject/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return(
+        <nav>
+            <ul className="nav-links">
+                <li> <a href="">home</a></li>
+                <li> <a href="">hats</a></li>
+                <li> <a href="">cards</a></li>
+                <li> <a href="">checkout</a></li>
+            </ul>
+           
+
+        </nav>
+    )
+}
+
+export default Navbar
