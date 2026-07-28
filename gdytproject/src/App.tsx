b64e17d3411ff43a8a63bd8cd40fa6fab5868467
jayden <jaydend082208@gmail.com>
2026-07-28 11:21:07 -0400

    we add more to are website

--- gdytproject/src/App.tsx
+++ gdytproject/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -7,6 +8,20 @@ function App() {
   return (
     <>
       <Navbar />
+      <div className="banner">
+        <h1>placeholder</h1>
+      </div>
+
+      <div className="main">
+        <h1>
+          trading cards & games, posters and more
+        </h1>
+        <p style={{color: 'black'}}>
+          Selling and trading 
+        </p>
+      </div>
+
+      <Footer />
       
 
 
---
+++ gdytproject/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>all rights reserves</p>
+        </footer>
+    )
+}
+export default Footer
+    
