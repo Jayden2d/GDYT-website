a12269a1104dcf90d795c66317b6ecc860bd2e98
jayden <jaydend082208@gmail.com>
2026-07-28 12:54:48 -0400

    saving because i need help

---
+++ gdytproject/src/cards.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function Cards(){
+    return(
+        <>
+          <Navbar />
+
+        </>
+    )
+}
