5a0eabbd329bb5171fe0727c1c7a04d0f01f52d0
jayden <jaydend082208@gmail.com>
2026-08-04 11:32:22 -0400

    we did are banner

--- gdytproject/src/Home.tsx
+++ gdytproject/src/Home.tsx
@@ -3,19 +3,21 @@ import ProductCard from "./components/ProductCard"
 function Home() {
   return (
     <>
-      <div className="banner">
-        <h1>placeholder</h1>
-      </div>
 
-      <div className="main">
+      <div className="banner">
         <h1>
           trading cards & games, posters and more
         </h1>
         <p style={{color: 'black'}}>
           Selling and trading
         </p>
+        <a href="" className="banner-button">
+          shop now
+        
+        </a>
         
       </div>
+
       <div className="main">
         <div className="content">
           <ProductCard />
